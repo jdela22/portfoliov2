8cf8b3709bf348ddc598476c343f917751c9778f
Jared Delapasse <jareddelapasse@gmail.com>
2024-09-07 20:32:48 -0500

    typing animation

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,15 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import profilepic from "../../public/profilepic.jpg";
+import { useEffect, useRef, useState } from "react";
+import { flushSync } from "react-dom";
 export default function Home() {
+  const name = "Jared Delapasse";
+  const index = useRef(0);
+  const intervalId = useRef<any>(0);
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    if (displayText === "") {
+      intervalId.current = setInterval(() => {
+        if (index.current == name.length) return;
+        flushSync(() => {
+          setDisplayText((prevText) => {
+            return (prevText += name[index.current]);
+          });
+        });
+        index.current++;
+      }, 100);
+    }
+
+    if (index.current == name.length) return clearInterval(intervalId.current);
+  }, [displayText]);
+
+  useEffect(() => {}, [displayText]);
+
   return (
     <>
-      <Image
-        src={profilepic}
-        alt="Picture of me"
-        width={200}
-        height={200}
-        className="rounded-full mb-4"
-      />
+      <div className="flex items-center">
+        <Image
+          src={profilepic}
+          alt="Picture of me"
+          width={200}
+          height={200}
+          className="rounded-full mb-4"
+        />
+        <h1 className="text-6xl ml-auto w-[576px]">
+          {displayText}
+          <span className="h-[50px] text-6xl animate-blink">|</span>
+        </h1>
+      </div>
       <div className="animate-slide-up">
         My name is Jared Delapasse. I'm a software engineer primarily focused on
         front-end. I've built this site to serve as a playground for the latest
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -69,11 +69,16 @@ module.exports = {
           "0%": { transform: "translateY(100%)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
         },
+        blink: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "slide-up": "slide-up 1s ease-out forwards",
+        blink: "blink 1s steps(2, end) infinite",
       },
     },
   },
